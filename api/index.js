34989e7712808a54d0e9e2288ddd2ae4614809b8
willysantoso05 <willysantoso05@gmail.com>
2021-04-20 22:09:20 +0700

    fix: linting

--- api/index.js
+++ api/index.js
@@ -3,24 +3,28 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+
 //import routes
 const authRoutes = require('./routes/auth');
 const { db } = require('./models/User');
+
 //app
 const app = express();
+
 // db
-mongoose
-  .connect(process.env.MONGODB_URI,{
+mongoose.connect(process.env.MONGODB_URI,{
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true
-   })
-  .then(() => console.log('DB Connected'));
+}).then(() => console.log('DB Connected'));
+
 //middlewares
 app.use(bodyParser.json());
 app.use(cors());
+
 //routes middleware
 app.use('/api', authRoutes);
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on ${port}`)
--- api/utils/auth.js
+++ api/utils/auth.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+
 exports.createJWT = (username, userId, duration) => {
    const payload = {
       username,
@@ -6,6 +7,6 @@ exports.createJWT = (username, userId, duration) => {
       duration
    };
    return jwt.sign(payload, process.env.TOKEN_SECRET, {
-     expiresIn: duration,
+      expiresIn: duration,
    });
 };
