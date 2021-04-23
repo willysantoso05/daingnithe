623195d20d56710efe8a2034852658cf4ded6639
willysantoso05 <willysantoso05@gmail.com>
2021-04-23 14:45:42 +0700

    feat: update users and middleware

---
+++ api/middleware/auth.js
@@ -0,0 +1,26 @@
+const JWT = require("jsonwebtoken");
+const User = require("../models/users");
+
+exports.auth = async (req, res, next) => {
+    try {
+        //get token from header: Bearer <token>
+        const token = req.headers.authorization.split(" ")[1];
+        console.log(token)
+
+        //verify this token was signed by your server
+        const decodedToken = JWT.verify(token, process.env.JWT_SECRET);
+        console.log(decodedToken.userId)
+        
+        ///Get user details
+        let user = await User.findById(decodedToken.userId);
+        if (!user) throw Error("Unauthenticated User");
+        
+        //put user in req object; so the controller can access current user
+        req.user = user;
+        next();
+    } catch {
+        return res.status(401).json({
+            message: "Unauthenticated"
+        });
+    }
+};
