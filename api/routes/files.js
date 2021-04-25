5100d1f918d23090b58acfa3ac73e45d56c4c8a1
willysantoso05 <willysantoso05@gmail.com>
2021-04-26 01:38:44 +0700

    feat: update files routes

--- api/routes/files.js
+++ api/routes/files.js
@@ -3,19 +3,19 @@ const router = express.Router();
 
 const getAllFilesController = require('../controllers/files/getAllFiles');
 const uploadFileController = require('../controllers/files/uploadFile');
-// const downloadFileController = require('../controllers/files/downloadFile');
-// const updateFileController = require('../controllers/files/updateFile');
-// const deleteFileController = require('../controllers/files/deleteFile');
-// const shareFileController = require('../controllers/files/shareFile');
+const downloadFileController = require('../controllers/files/downloadFile');
+const updateFileController = require('../controllers/files/updateFile');
+const deleteFileController = require('../controllers/files/deleteFile');
+const shareFileController = require('../controllers/files/shareFile');
 
 
 router.get('/', getAllFilesController.getAllFiles);
 router.post('/', uploadFileController.uploadFile);
 // router.get('/:fileId', downloadFileController);
 
-// router.put('/:fileId', updateFileController);
-// router.delete('/:fileId', deleteFileController);
+router.put('/:fileId', updateFileController.updateFile);
+router.delete('/:fileId', deleteFileController.deleteFile);
 
-// router.put('/access/:fileId', shareFileController);
+router.put('/access/:fileId', shareFileController.shareFile);
 
 module.exports = router;
