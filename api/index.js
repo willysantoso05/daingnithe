6c936ce8b3191b274a85c6be8c1a79a270bf7b34
willysantoso05 <willysantoso05@gmail.com>
2021-04-27 22:18:10 +0700

    fix: minor update fixes

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const logger = require('morgan');
-const users = require('./routes/users');
-const files = require('./routes/files');
 const bodyParser = require('body-parser');
 const mongoose = require('./config/database');
+const users = require('./routes/users');
+const files = require('./routes/files');
 const jwtAuth = require('./middleware/auth');
-const fileUpload = require('express-fileupload');
 
 const app = express();
+
 // connection to mongodb
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use(logger('dev'));
@@ -27,9 +28,9 @@ app.use('/file', jwtAuth.auth, files);
 // express doesn't consider not found 404 as an error so we need to handle 404 explicitly
 // handle 404 error
 app.use(function(req, res, next) {
- let err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+	let err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // handle errors
@@ -42,6 +43,6 @@ app.use(function(err, req, res, next) {
 		res.status(500).json({message: "Something looks wrong :( !!!"});
 });
 
-app.listen(3000, function(){
-	console.log('Node server listening on port 3000');
+app.listen(process.env.API_PORT, function(){
+	console.log(`Node server listening on port ${process.env.API_PORT}`);
 });
