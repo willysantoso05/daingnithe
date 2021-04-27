b53c113b4e22ec192dfa193e2f212494684d6797
willysantoso05 <willysantoso05@gmail.com>
2021-04-27 22:56:07 +0700

    fix: fixes on key contract

--- blockchain/chaincode/src/keyAssetContract.js
+++ blockchain/chaincode/src/keyAssetContract.js
@@ -80,28 +80,29 @@ class keyAssetContract extends Contract {
         return ctx.stub.deleteState(id);
     }
 
+    //DEBUG ONLY
     // GetAllAssets returns all assets found in the world state.
-    async GetAllKeyAssets(ctx) {
-        const allResults = [];
-        // range query with empty string for startKey and endKey does an open-ended query of all assets in the chaincode namespace.
-        const iterator = await ctx.stub.getStateByRange('', '');
-        let result = await iterator.next();
-        while (!result.done) {
-            const strValue = Buffer.from(result.value.value.toString()).toString('utf8');
-            let record;
-            try {
-                record = JSON.parse(strValue);
-            } catch (err) {
-                console.log(err);
-                record = strValue;
-            }
-            if (Object.keys(record).length == 7){
-                allResults.push({ Key: result.value.key, Record: record });
-            }
-            result = await iterator.next();
-        }
-        return JSON.stringify(allResults);
-    }
+    // async GetAllKeyAssets(ctx) {
+    //     const allResults = [];
+    //     // range query with empty string for startKey and endKey does an open-ended query of all assets in the chaincode namespace.
+    //     const iterator = await ctx.stub.getStateByRange('', '');
+    //     let result = await iterator.next();
+    //     while (!result.done) {
+    //         const strValue = Buffer.from(result.value.value.toString()).toString('utf8');
+    //         let record;
+    //         try {
+    //             record = JSON.parse(strValue);
+    //         } catch (err) {
+    //             console.log(err);
+    //             record = strValue;
+    //         }
+    //         if (Object.keys(record).length == 7){
+    //             allResults.push({ Key: result.value.key, Record: record });
+    //         }
+    //         result = await iterator.next();
+    //     }
+    //     return JSON.stringify(allResults);
+    // }
 }
 
 module.exports = keyAssetContract;
