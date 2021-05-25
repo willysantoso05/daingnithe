4a2426d52e78366f2629b91433bad5314994a461
willysantoso05 <willysantoso05@gmail.com>
2021-05-25 14:29:55 +0700

    feat: add get and upload file pages

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -6,6 +6,27 @@
       <h2> User : <strong>{{username}}</strong></h2>
       <br>
       <h4> Files </h4>
+
+      <form @submit.prevent="submitFile">
+        <h1 class="h3 mb-3 fw-normal">Upload New File</h1>
+        <label>Upload a new file</label>
+        <input @change="onFileChange" type="file" class="form-control" placeholder="File">
+
+        <button class="w-100 btn btn-lg btn-primary mt-4" type="submit">Upload</button>
+      </form>
+
+      <ul v-if="files">
+        <li v-for="file in files" :key="file.id">
+          <ul>
+            <li>
+              <span class="fileName"> {{file.FileName}} </span>
+              <span class="ownerFile"> {{file.OwnerFile}} </span>
+              <!-- <span class="fileName"> {{file.FileName}} </span> -->
+              <span class="lastUpdated"> {{file.LastUpdated}} </span>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -16,12 +37,47 @@ import axios from 'axios';
 
 export default {
   name: "Home",
-  async updated() {
-    const response = await axios.get('file');
-    console.log(response);
+  data(){
+    return {
+      newFile: null,
+      files: null
+    }
   },
+  async created() {
+    if (this.userID && this.username){
+      const response = await axios.get('file');
+
+      let result = null
+      if (response.data.data.length != 0){
+        result = response.data.data
+      }
+      console.log(response);
+
+      this.files = result
+    }
+  },
+  
   computed: {
-    ...mapGetters(['userID', 'username'])
+    ...mapGetters(['userID', 'username']),
+  },
+
+  methods: {
+    onFileChange(event) {
+      this.newFile = event.target.files[0]
+      console.log(event.target.files)
+    },
+    async submitFile() {
+      const headers = { 
+        "Content-Type": "multipart/form-data; boundary=<calculated when request is sent>"
+      }
+
+      var form = new FormData();
+      form.append('file',this.newFile);
+
+      const response = await axios.post('/file', form, {headers: headers});
+      console.log(response);
+      this.$router.go()
+    }
   }
 }
 </script>
